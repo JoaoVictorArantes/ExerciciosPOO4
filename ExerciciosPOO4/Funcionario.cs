9c888b2958bcd09e80e466d83ebc592cc05f0187
João Victor <jvaarantes@gmail.com>
2024-01-25 11:47:44 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosPOO4/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosPOO4
+{
+    internal class Funcionario
+    {
+        public String Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double ImpostoPorcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * (ImpostoPorcentagem / 100));
+        }
+        public override string ToString()
+        {
+            return Nome + ", $" + SalarioBruto.ToString("F2");
+        }
+    }
+}
---
+++ ExerciciosPOO4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ExerciciosPOO4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Fazer um programa para ler os dados de um funcionário 
+             * (nome,salário bruto e imposto). Em seguida,
+             * mostrar os dados do funcionário (nome e salário líquido).
+             * Em seguida, aumentar o salário do funcionário com base 
+             * em uma porcentagem dada (somente o salário bruto é afetado 
+             * pela porcentagem) e mostrar novamente os dados do funcionário.
+             * Use a classe projetada abaixo.*/
+
+            Funcionario funcionario = new Funcionario();
+
+            Console.WriteLine("Digite os dados do funcionário: ");
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Imposto em reais: ");
+            funcionario.Imposto = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Funcionário: {funcionario}");
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = Convert.ToDouble(Console.ReadLine());
+            funcionario.AumentarSalario(porcentagem);
+
+            Console.WriteLine($"Dados atualizados: {funcionario}");
+
+            Console.ReadKey();
+        }
+    }
+}
